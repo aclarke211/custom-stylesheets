8adbfc76f92c1280e7f09a0e5e6b481c410ce0d6
Alex Clarke <aclarke211@gmail.com>
2019-06-23 17:43:57 +0100

    Add a stylesheet when button is clicked.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const testBtn = document.getElementsByClassName('cs-test-btn')[0];
 
 testBtn.addEventListener('click', () => {
-  alert('Test Button Clicked.');
+  console.log('Adding stylesheets.');
+
+  var sheet = document.createElement('style')
+  sheet.innerHTML = `
+    .cs-test-btn {
+      background-color: blue !important;
+    }
+  `;
+  sheet.classList.add('external-stylesheet');
+  document.body.appendChild(sheet);
 });
